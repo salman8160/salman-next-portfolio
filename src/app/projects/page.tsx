e448e6bb104ae1c834f972335fd042fb1b3ea7d4
Salman Tabrez <mdsalmantabrez@gmail.com>
2025-07-01 13:57:03 -0500

    Fix: added projects/page.tsx to handle route

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+// src/app/projects/page.tsx
+
+'use client';
+import Projects from '@/components/Projects';
+
+export default function ProjectsPage() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Projects />
+    </div>
+  );
+}
